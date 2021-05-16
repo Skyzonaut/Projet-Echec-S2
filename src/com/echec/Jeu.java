f34431f32eafd4ee7c81af2bce5edb1bfc82a04c
yohan <yohan.folliot@gmail.com>
2021-05-16 11:55:15 +0200

    Ajout des gestions de commandes prendrePiece et deplacerPiece

--- src/com/echec/Jeu.java
+++ src/com/echec/Jeu.java
@@ -2,11 +2,24 @@ package com.echec;
 
 import com.echec.game.*;
 
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Jeu {
 
-    public PlateauDeJeu jouer() {
+    PlateauDeJeu plateau;
+    public static final String[] arrayDesCommandes = {"deplacer", "prendre"};
+    public static final List<String> listeDesCommandes = Arrays.asList(arrayDesCommandes);
 
-        PlateauDeJeu plateau = new PlateauDeJeu();
+    public Jeu() {
+        this.plateau = new PlateauDeJeu();
+    }
+
+    public PlateauDeJeu jouer() {
         return plateau;
     }
 
@@ -17,17 +30,88 @@ public class Jeu {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
 
+    {
         Jeu jeu = new Jeu();
         PlateauDeJeu plateau = jeu.jouer();
         plateau.afficher();
-        plateau.deplacerPiece(plateau.getGrille().getCase(2, 2), plateau.getGrille().getCase(2, 4));
-        plateau.afficher();
-        plateau.prendrePiece(plateau.getGrille().getCase(2, 3), plateau.getGrille().getCase(2, 1));
-        plateau.afficher();
 
+        boolean jeuEnCours = true;
 
+        while (jeuEnCours)
+        {
+
+            String commande = getCommandeInput();
+
+            if (listeDesCommandes.contains(commande))
+            {
+                Case origine = jeu.getCaseDepuisCoordonneeInput();
+                Case destination = jeu.getCaseDepuisCoordonneeInput();
+
+                switch (commande)
+                {
+                    case "prendre" : plateau.prendrePiece(origine, destination);
+                        break;
+                    case "deplacer" : plateau.deplacerPiece(origine, destination);
+                        break;
+                    default: System.out.println("Commande non reconnu");
+                }
+
+            }
+            else
+            {
+                System.out.println("Commande incompréhensible");
+            }
+            plateau.afficher();
+        }
     }
 
+    public static String getCommandeInput() {
+
+        Scanner inputCommandeScanner = new Scanner(System.in);
+        System.out.println("[deplacer/prendre]");
+        String commande = inputCommandeScanner.nextLine();
+        return commande;
+    }
+
+    public Case getCaseDepuisCoordonneeInput() {
+
+        Scanner inputCoordDestinataireScanner = new Scanner(System.in);
+        System.out.println("Destination : [xy ou x y]");
+        String coordDestination = getCoordonneeBonFormat(inputCoordDestinataireScanner);
+        Case destination = convertirCaseDepuisInput(coordDestination, this.plateau.getGrille());
+        return destination;
+    }
+
+    public static String getCoordonneeBonFormat(Scanner scanner) {
+
+        Pattern pattern = Pattern.compile("^\\d \\d$|^\\d\\d$");
+        Matcher matcher;
+
+        String coord = scanner.nextLine();
+        matcher = pattern.matcher(coord);
+
+        if (!matcher.find()) {
+            System.out.println("Le format est [x y] ou [xy]");
+            while (!matcher.find()) {
+                coord = scanner.nextLine();
+                matcher = pattern.matcher(coord);
+            }
+        }
+        return coord;
+    }
+
+    public static Case convertirCaseDepuisInput(String inputString, Grille grille) {
+        int x;
+        int y;
+        if (inputString.contains(" ")) {
+            x = Integer.parseInt(inputString.split(" ")[0]);
+            y = Integer.parseInt(inputString.split(" ")[1]);
+        } else {
+            x = Integer.parseInt(inputString.split("")[0]);
+            y = Integer.parseInt(inputString.split("")[1]);
+        }
+        return grille.getCase(x, y);
+    }
 }
