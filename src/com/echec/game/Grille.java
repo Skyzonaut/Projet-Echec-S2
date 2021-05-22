bb0bd0e23232da8814dd469234524146e1f39798
yohan <yohan.folliot@gmail.com>
2021-05-22 17:22:09 +0200

    Correction du placement des pions

--- src/com/echec/game/Grille.java
+++ src/com/echec/game/Grille.java
@@ -42,35 +42,35 @@ public class Grille {
     public void initialiserGrille() {
 
         // Blanc
-        this.getCase(1, 1).ajouterPiece(new Tour("Tour_Noir_1", "noir", true));
-        this.getCase(2, 1).ajouterPiece(new Fou("Fou_Noir_1", "noir", true));
-        this.getCase(3, 1).ajouterPiece(new Cavalier("Cavalier_Noir_1", "noir", true));
-        this.getCase(4, 1).ajouterPiece(new Reine("Reine_Noir_1", "noir", true));
-        this.getCase(5, 1).ajouterPiece(new Roi("Roi_Noir_1", "noir", true));
-        this.getCase(6, 1).ajouterPiece(new Fou("Fou_Noir_2", "noir", true));
-        this.getCase(7, 1).ajouterPiece(new Cavalier("Cavalier_Noir_2", "noir", true));
-        this.getCase(8, 1).ajouterPiece(new Tour("Tour_Noir_2", "noir", true));
+        this.getCase(1, 8).ajouterPiece(new Tour("Tour_Noir_1", "noir", true));
+        this.getCase(2, 8).ajouterPiece(new Fou("Fou_Noir_1", "noir", true));
+        this.getCase(3, 8).ajouterPiece(new Cavalier("Cavalier_Noir_1", "noir", true));
+        this.getCase(4, 8).ajouterPiece(new Reine("Reine_Noir_1", "noir", true));
+        this.getCase(5, 8).ajouterPiece(new Roi("Roi_Noir_1", "noir", true));
+        this.getCase(6, 8).ajouterPiece(new Fou("Fou_Noir_2", "noir", true));
+        this.getCase(7, 8).ajouterPiece(new Cavalier("Cavalier_Noir_2", "noir", true));
+        this.getCase(8, 8).ajouterPiece(new Tour("Tour_Noir_2", "noir", true));
 
         // Pions blancs
         for (int x = 1; x <= 8; x++) {
-            this.getCase(x, 2).ajouterPiece(new Pion(String.format("Pion_Noir_%s", x),
+            this.getCase(x, 7).ajouterPiece(new Pion(String.format("Pion_Noir_%s", x),
                     "noir", true));
         }
 
         // Noir
-        this.getCase(1, 8).ajouterPiece(new Tour("Tour_blanc_1", "blanc", true));
-        this.getCase(2, 8).ajouterPiece(new Fou("Fou_blanc_1", "blanc", true));
-        this.getCase(3, 8).ajouterPiece(new Cavalier("Cavalier_blanc_1", "blanc", true));
-        this.getCase(4, 8).ajouterPiece(new Reine("Reine_blanc_1", "blanc", true));
-        this.getCase(5, 8).ajouterPiece(new Roi("Roi_blanc_1", "blanc", true));
-        this.getCase(6, 8).ajouterPiece(new Fou("Fou_blanc_2", "blanc", true));
-        this.getCase(7, 8).ajouterPiece(new Cavalier("Cavalier_blanc_2", "blanc", true));
-        this.getCase(8, 8).ajouterPiece(new Tour("Tour_blanc_2", "blanc", true));
+        this.getCase(1, 1).ajouterPiece(new Tour("Tour_blanc_1", "blanc", true));
+        this.getCase(2, 1).ajouterPiece(new Fou("Fou_blanc_1", "blanc", true));
+        this.getCase(3, 1).ajouterPiece(new Cavalier("Cavalier_blanc_1", "blanc", true));
+        this.getCase(4, 1).ajouterPiece(new Reine("Reine_blanc_1", "blanc", true));
+        this.getCase(5, 1).ajouterPiece(new Roi("Roi_blanc_1", "blanc", true));
+        this.getCase(6, 1).ajouterPiece(new Fou("Fou_blanc_2", "blanc", true));
+        this.getCase(7, 1).ajouterPiece(new Cavalier("Cavalier_blanc_2", "blanc", true));
+        this.getCase(8, 1).ajouterPiece(new Tour("Tour_blanc_2", "blanc", true));
 
 
         // Pions noirs
         for (int x = 1; x <= 8; x++) {
-            this.getCase(x, 7).ajouterPiece(new Pion(String.format("Pion_Blanc_%s", x),
+            this.getCase(x, 2).ajouterPiece(new Pion(String.format("Pion_Blanc_%s", x),
                     "blanc", true));
         }
     }
