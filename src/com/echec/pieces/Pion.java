ec500db2eae731a398099a8807ac795b0ff715fa
yohan <yohan.folliot@gmail.com>
2021-05-22 22:40:11 +0200

    Javadoc

--- src/com/echec/pieces/Pion.java
+++ src/com/echec/pieces/Pion.java
@@ -3,10 +3,23 @@ import com.echec.game.Case;
 import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Pion d'echec, hérité directement de {@linkplain Piece}
+ * @see com.echec.pieces.Piece
+ * @author yohan
+ */
 public class Pion extends Piece {
 
+    /**
+     * Constructeur champ à champ d'une pièce de Pion
+     * @param id String : Identifiant du pion
+     * @param couleur String : Noir ou Blanc, couleur de la pièce
+     * @param etat Boolean : état de la pièce, est-elle prise ou non <b>Not in Use</b>
+     * @see Pion#Pion(JSONObject)
+     * @see Piece
+     * @author yohan
+     */
     public Pion(String id, String couleur, boolean etat) {
         this.id = id;
         this.couleur = couleur;
@@ -15,6 +28,14 @@ public class Pion extends Piece {
         this.pieceBlancheUTF = "♟";
     }
 
+    /**
+     * Constructeur par recopie d'une pièce de Pion depuis une sauvegarde
+     * @param jsonObject JSONObject : Objet Json contenant la pièce sauvegardée d'une autre partie, contenu dans
+     *                   un fichier Json de sauvegarde
+     * @see Pion#Pion(String, String, boolean)
+     * @see Piece
+     * @author yohan
+     */
     public Pion(JSONObject jsonObject) {
         this.id = (String) jsonObject.get("id");
         this.couleur = (String) jsonObject.get("couleur");
@@ -23,13 +44,12 @@ public class Pion extends Piece {
         this.pieceBlancheUTF = "♟";
     }
 
+    /**
+     * <i>En développement</i>
+     * @param mode String
+     * @return Liste des cases où pourra se déplacer le pion
+     */
     public ArrayList<Case> getDeplacement(String mode) {
-        ArrayList<Case> listeCasesDeplacement = new ArrayList<Case>();
-
-        return listeCasesDeplacement;
+        return new ArrayList<>();
     }
-
-    public Pion() {
-    }
-
 }
--- src/com/echec/pieces/Roi.java
+++ src/com/echec/pieces/Roi.java
@@ -4,8 +4,22 @@ import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 
+/**
+ * Roi d'echec, hérité directement de {@linkplain Piece}
+ * @see com.echec.pieces.Piece
+ * @author yohan
+ */
 public class Roi extends Piece {
 
+    /**
+     * Constructeur champ à champ d'une pièce de Roi
+     * @param id String : Identifiant du Roi
+     * @param couleur String : Noir ou Blanc, couleur de la pièce
+     * @param etat Boolean : état de la pièce, est-elle prise ou non <b>Not in Use</b>
+     * @see Roi#Roi(JSONObject)
+     * @see Piece
+     * @author yohan
+     */
     public Roi(String id, String couleur, boolean etat) {
         this.id = id;
         this.couleur = couleur;
@@ -13,7 +27,15 @@ public class Roi extends Piece {
         this.pieceNoirUTF = "♔";
         this.pieceBlancheUTF = "♚";
     }
-
+    
+    /**
+     * Constructeur par recopie d'une pièce de Roi depuis une sauvegarde
+     * @param jsonObject JSONObject : Objet Json contenant la pièce sauvegardée d'une autre partie, contenu dans
+     *                   un fichier Json de sauvegarde
+     * @see Roi#Roi(String, String, boolean)
+     * @see Piece
+     * @author yohan
+     */
     public Roi(JSONObject jsonObject) {
         this.id = (String) jsonObject.get("id");
         this.couleur = (String) jsonObject.get("couleur");
@@ -22,14 +44,12 @@ public class Roi extends Piece {
         this.pieceBlancheUTF = "♚";
     }
 
+    /**
+     * <i>En développement</i>
+     * @param mode String
+     * @return Liste des cases où pourra se déplacer le Roi
+     */
     public ArrayList<Case> getDeplacement(String mode) {
-        ArrayList<Case> listeCasesDeplacement = new ArrayList<Case>();
-
-        return listeCasesDeplacement;
+        return new ArrayList<>();
     }
-
-
-    public Roi() {
-    }
-
 }
