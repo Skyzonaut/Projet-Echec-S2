4ae81abedffc3d7a40af4b66bf9fbe5701806af2
yohan <yohan.folliot@gmail.com>
2021-06-05 11:55:51 +0200

    Gestion des Echecs comme évènement unique

--- src/com/echec/game/Echec.java
+++ src/com/echec/game/Echec.java
@@ -5,13 +5,16 @@ import java.util.HashMap;
 
 public class Echec {
     private Case roi;
+    private String couleur;
     private PlateauDeJeu plateau;
+    private String couleurRoi;
     private ArrayList<Deplacement> sauveur = new ArrayList<>();
     private ArrayList<Deplacement> attaquants = new ArrayList<>();
 
-    public Echec (PlateauDeJeu plateau, Case roi) {
+    public Echec (PlateauDeJeu plateau, Case roi, String couleur) {
         this.plateau = plateau;
         this.roi = roi;
+        this.couleurRoi = couleur;
     }
 
     public void addAttaquant(Deplacement d) {
@@ -23,8 +26,36 @@ public class Echec {
     }
 
     public void trouverSauveur() {
+//        String couleurEnnemie = roi.piece.getCouleur().equals("noir") ? "blanc" : "noir";
+        System.out.println("---------------------------------------------------------------------------");
+        if (couleurRoi.equalsIgnoreCase("blanc")) {
+            for (Deplacement d : attaquants) {
+                for (Case c : this.plateau.getCheminToRoi(d.getOrigine(), this.roi)) {
+                    for (Deplacement s : this.plateau.listeDeplacementBlancs) {
+                        if (s.contains(c)) {
+                            if (!sauveur.contains(s)) {
+                                sauveur.add(s);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            for (Deplacement d : attaquants) {
+                for (Case c : this.plateau.getCheminToRoi(d.getOrigine(), this.roi)) {
+                    for (Deplacement s : this.plateau.listeDeplacementNoirs) {
+                        if (s.contains(c)) {
+                            if (!sauveur.contains(s)) {
+                                sauveur.add(s);
+                            }
+                        }
+                    }
+                }
+            }
+        }
         for (Deplacement d : sauveur) {
-
+            System.out.println(d);
         }
     }
 }
