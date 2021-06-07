ad1f9e16e6aa6bc8d83d53139218b613d3d8ab23
yohan <yohan.folliot@gmail.com>
2021-06-07 04:57:28 +0200

    Release Version + Doc

--- src/com/echec/game/Jeu.java
+++ src/com/echec/game/Jeu.java
@@ -2,6 +2,7 @@ package com.echec.game;
 
 import com.echec.Tools;
 import com.echec.pieces.Piece;
+import com.echec.ui.Controller;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -17,11 +18,35 @@ import java.util.*;
 
 public class Jeu {
 
+    /**
+     * Plateau de jeu dans lequel sera contenu l'essentiel des données et des opérations
+     * {@linkplain PlateauDeJeu}
+     */
     public PlateauDeJeu plateau;
+
+    /**
+     * Liste static des commandes existantes
+     */
     public static final String[] arrayDesCommandes = {"deplacer", "prendre", "save", "charger", "undo", "quitter", "abandonner", "help", "historique"};
+
+    /**
+     * Liste des raccourcis des commandes existantes
+     */
     public static final String[] arrayDesCommandesRaccourci = {"d", "p", "s", "c", "u", "q", "a", "h", "n"};
+
+    /**
+     * ArrayList des commandes existantes
+     */
     public static final List<String> listeDesCommandes = Arrays.asList(arrayDesCommandes);
+
+    /**
+     * ArrayList des raccourcis des commandes existantes
+     */
     public static final List<String> listeDesCommandesRaccourci = Arrays.asList(arrayDesCommandesRaccourci);
+
+    /**
+     * Tour actuel du jeu. Pour commencer il est mis à 0 comme aux règles standards des echecs
+     */
     private String tour = "blanc";
 
     /**
@@ -29,9 +54,20 @@ public class Jeu {
      * 2 : facile
      */
     private int niveauDeDifficulte;
+
+    /**
+     * FileWriter servant à l'écriture des sauvegarde des fichiers
+     */
     private static FileWriter fw;
+
+    /**
+     * Variable de maintient de vie du jeu
+     */
     private Boolean jeuEnCours = true;
 
+    /**
+     * Constructeur par défaut du Jeu
+     */
     public Jeu() {
         this.plateau = new PlateauDeJeu();
     }
@@ -41,6 +77,12 @@ public class Jeu {
 //        jeu.jouer();
 //    }
 
+    /**
+     * Fonction assurant le cycle de jeu du jeu au format textuel, ainsi que l'analyse des input et leur
+     * retranscription en commandes
+     * @throws Exception Any
+     * @author yohan
+     */
     public void jouer() throws Exception {
 
         while (jeuEnCours) {
@@ -119,6 +161,12 @@ public class Jeu {
         }
     }
 
+    /**
+     * Fonction permettant de prendre la pièce saisie depuis la case saisie.
+     * @see Case
+     * @see com.echec.ui.EchecApplication
+     * @author yohan
+     */
     public String prendrePiece() {
         Case origine = this.getCaseOrigineDepuisCoordonneeInput();
         Case destination = this.getCaseDestinationDepuisCoordonneeInput();
@@ -131,6 +179,12 @@ public class Jeu {
         }
     }
 
+    /**
+     * Fonction permettant de déplacer les pièces depuis les cases saisies.
+     * @see Case
+     * @see com.echec.ui.EchecApplication
+     * @author yohan
+     */
     public String deplacerPiece() {
         Case origine = this.getCaseOrigineDepuisCoordonneeInput();
         Case destination = this.getCaseDestinationDepuisCoordonneeInput();
@@ -143,6 +197,13 @@ public class Jeu {
         }
     }
 
+    /**
+     * Fonction annulant un coup.
+     * @see Historique
+     * @see Evenement
+     * @see Controller#undo()
+     * @author yohan
+     */
     public void undo() {
         System.out.println("undo done");
         Evenement dernierEvenement = this.plateau.historique.getDernierEvenement("undo");
@@ -164,6 +225,15 @@ public class Jeu {
         this.tour = (this.tour.equals("blanc")) ? "noir" : "blanc";
     }
 
+    /**
+     * Fonction sauvegardant le jeu dans un fichier interne après avoir proposé au joueur de nommer sa sauvegarde
+     * @return <code>String</code> Chaîne de confirmation
+     * @see Controller#menuItemChargerOnClick()
+     * @see PlateauDeJeu
+     * @see Jeu#saveFromUi(File)
+     * @see Jeu#getSaveFileName(String)
+     * @author yohan
+     */
     public String save() {
         String dossierSauvegardeChemin = "../save/";
         File repertoire = new File(dossierSauvegardeChemin);
@@ -196,6 +266,15 @@ public class Jeu {
         return "Partie sauvegardé : " + nouveauFichierChemin;
     }
 
+    /**
+     * Fonction sauvegardant sur un fichier le contenu du jeu
+     * @param file <code>File</code> Chemin du fichier dans lequel sauvegarder le jeu
+     * @see Controller#menuItemChargerOnClick()
+     * @see PlateauDeJeu
+     * @see Jeu#saveFromUi(File)
+     * @see Jeu#getSaveFileName(String)
+     * @author yohan
+     */
     public void saveFromUi(File file) {
         JSONObject jsonObject = this.plateau.getJSONObject();
         jsonObject.put("tour", this.tour);
@@ -203,6 +282,16 @@ public class Jeu {
         writeFile(file.getPath(), jsonObject);
     }
 
+    /**
+     * Fonction écrivant le contenu du jeu au format <code>JSONObject</code> sur un fichier de chemin donné
+     * @param filePath <code>String</code> chemin où écrire le fichier
+     * @param jsonObject <code>JSONObject</code> sauvegarde du jeu au format JSON
+     * @see Controller#menuItemChargerOnClick()
+     * @see PlateauDeJeu
+     * @see Jeu#saveFromUi(File)
+     * @see Jeu#getSaveFileName(String)
+     * @author yohan
+     */
     public void writeFile(String filePath, JSONObject jsonObject) {
         try {
             fw = new FileWriter(filePath);
@@ -219,12 +308,21 @@ public class Jeu {
         }
     }
 
+    /**
+     * Fonction chargeant la sauvegarde sélectionnée par l'utilisateur, et réinitialisant le jeu avec celle-ci. 
+     * <p>Avec la fonction {@linkplain Jeu#chargerJeuFromFile(File)}</p>
+     * @return <code>String</code> Chaîne de confirmation
+     * @see Controller#menuItemChargerOnClick() 
+     * @see Jeu#writeFile(String, JSONObject)
+     * @see Jeu#save()
+     * @author yohan
+     */
     public String charger() {
-
         // On demande si l'utilisateur veut sauvegarder sa partie avant
         System.out.println("Vous allez écraser votre partie - Voulez vous la sauvegarder ?");
         String ouiNon = Tools.getOuiNon();
-        if (ouiNon.equalsIgnoreCase("oui")) {
+        if (ouiNon.equalsIgnoreCase("oui"))
+        {
             System.out.println(this.save() + "\n");
         }
 
@@ -235,12 +333,15 @@ public class Jeu {
         // On affiche la liste des sauvegardes pour sélection
         System.out.println("Sélectionnez la sauvegarde que vous souhaitez utiliser :\n");
 
-        if (repertoire.isDirectory()) {
+        if (repertoire.isDirectory()) 
+        {
             File[] listeFiles = repertoire.listFiles();
             Arrays.sort(listeFiles, Comparator.comparingLong(File::lastModified));
-            if (listeFiles.length != 0) {
+            if (listeFiles.length != 0)
+            {
                 int indexFile = 0;
-                for (File file : listeFiles) {
+                for (File file : listeFiles)
+                {
                     System.out.println(indexFile + " - " + getSaveFileName(file.getName()));
                     indexFile++;
                 }
@@ -252,14 +353,28 @@ public class Jeu {
                 // On charge le fichier sélectionné et on le transforme en json
                 this.chargerJeuFromFile(listeFiles[sauvegardeChoisie]);
 
-            } else {
+            }
+            else
+            {
                 stringRetour = "Aucune sauvegarde n'a été trouvée";
             }
-        } else { stringRetour = "Dossier de sauvegarde non trouvé"; }
+        }
+        else
+        {
+            stringRetour = "Dossier de sauvegarde non trouvé";
+        }
         stringRetour = "Partie chargée";
         return stringRetour;
     }
-
+    
+    /**
+     * Fonction initilisant un nouveau jeu avec toutes ses données depuis un fichier de sauvegarde au format JSON.
+     * @see Jeu#charger()
+     * @see Controller#menuItemChargerOnClick()
+     * @see Jeu#writeFile(String, JSONObject)
+     * @see Jeu#save()
+     * @author yohan
+     */
     public void chargerJeuFromFile(File file) {
         try {
             Object obj = new JSONParser().parse(new FileReader(file));
@@ -273,13 +388,21 @@ public class Jeu {
         }
     }
 
+    /**
+     * Fonction lançant une partie et demandant les paramètres du jeu à l'utilisateur
+     * @return <code>int[3]</code> une liste de paramètres de jeu
+     * @see Jeu#finirPartie()
+     * @see Jeu#quitterJeu()
+     * @author yohan
+     */
     public int[] lancerPartie() {
         int[] listeParam = new int[3];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Sélectionnez le niveau de difficulté");
         System.out.println("\n1 - Normal : Règles standards\n2 - Apprenti : Possibilité de retour en arrière");
         listeParam[0] = scanner.nextInt();
-        while (listeParam[0] > 2 ||  listeParam[0] < 1) {
+        while (listeParam[0] > 2 ||  listeParam[0] < 1)
+        {
             System.out.println("Choisissez une difficult disponible!");
             listeParam[0] = scanner.nextInt();
         }
@@ -287,18 +410,36 @@ public class Jeu {
         return listeParam;
     }
 
+    /**
+     * Fonction terminant la partie après avoir proposé de sauvegarder
+     * @see Jeu#lancerPartie()
+     * @see Jeu#quitterJeu()
+     * @see Jeu#save()
+     * @author yohan
+     */
     public Boolean finirPartie() {
         System.out.println("[Voulez vous sauvegarder votre partie ?]");
         if (Tools.getOuiNon().equalsIgnoreCase("oui")) this.save();
         return false;
     }
 
+    /**
+     * Fonction quittant le jeu après avoir proposé de sauvegarder
+     * @see Jeu#lancerPartie()
+     * @see Jeu#finirPartie()
+     * @see Jeu#save()
+     * @author yohan
+     */
     public void quitterJeu() {
         System.out.println("[Voulez vous sauvegarder votre partie ?]");
         if (Tools.getOuiNon().equalsIgnoreCase("oui")) this.save();
         System.exit(0);
     }
 
+    /**
+     * Fonction affichant les commandes et leurs foncitonnement pour l'interface textuelle (console)
+     * @author yohan
+     */
     public void help() {
         System.out.println("[Déplacer] " + "-".repeat(40) +
                 "| Entrez : deplacer ou d\n" +
@@ -360,6 +501,13 @@ public class Jeu {
                 "| Entrez : historique ou n\n" +
                 "| Commande permettant d'afficher l'historique des coups et undo\n");
     }
+
+    /**
+     * Fonction demandant et récupérant une commande auprès de l'utilisateur
+     * @return <code>String</code> commande rentrée par l'utlisateur
+     * @see Jeu#printListeCommande()
+     * @author yohan
+     */
     public String getCommandeInput() {
         printListeCommande();
         Scanner inputCommandeScanner = new Scanner(System.in);
@@ -367,14 +515,31 @@ public class Jeu {
         return commande;
     }
 
+    /**
+     * Fonction affichant sur la console la liste des commandes et de leur raccourci
+     * @see Jeu#arrayDesCommandes
+     * @see Jeu#arrayDesCommandesRaccourci
+     * @author Yohan
+     */
     public void printListeCommande() {
-        String strCommande = "[";
-        for (String commande : arrayDesCommandes) {strCommande += commande + "/";}
-        strCommande += "] ou leurs raccourcis : [";
-        for (String commande : arrayDesCommandesRaccourci) {strCommande += commande + "/";}
+        StringBuilder strCommande = new StringBuilder("[");
+        for (String commande : arrayDesCommandes) {
+            strCommande.append(commande).append("/");}
+        strCommande.append("] ou leurs raccourcis : [");
+        for (String commande : arrayDesCommandesRaccourci) {
+            strCommande.append(commande).append("/");}
         System.out.println(strCommande + "]");
     }
 
+    /**
+     * Fonction récupérant une case d'origine d'un mouvement quelconque depuis les coordonnées
+     * successivement rentrée par l'utilisateur
+     * @return <code>{@linkplain Case}</code> case correspondant aux coordonnées rentrées
+     * @author Yohan
+     * @see Tools#getCoordonneeBonFormat(Scanner) 
+     * @see Jeu#convertirCaseDepuisInput(String, Grille)
+     * @author Yohan
+     */
     public Case getCaseOrigineDepuisCoordonneeInput() {
         Scanner inputCoordDestinataireScanner = new Scanner(System.in);
         System.out.println("Origine : [xy ou x y] ou [ay ou a y]");
@@ -383,6 +548,15 @@ public class Jeu {
         return destination;
     }
 
+    /**
+     * Fonction récupérant une case de destination d'un mouvement quelconque depuis les coordonnées
+     * successivement rentrée par l'utilisateur
+     * @return <code>{@linkplain Case}</code> case correspondant aux coordonnées rentrées
+     * @author Yohan
+     * @see Tools#getCoordonneeBonFormat(Scanner)
+     * @see Jeu#convertirCaseDepuisInput(String, Grille)
+     * @author Yohan
+     */
     public Case getCaseDestinationDepuisCoordonneeInput() {
 
         Scanner inputCoordDestinataireScanner = new Scanner(System.in);
@@ -392,6 +566,15 @@ public class Jeu {
         return destination;
     }
 
+    /**
+     * Fonction récupérant une case depuis les coordonnées textuelles d'une case
+     * @param inputString <code>String</code> id de la case au format <code>String</code>
+     * @param grille <code>{@linkplain Grille}</code> grille dans laquelle est contenue la case recherchée
+     * @return <code>{@linkplain Case}</code> Case correspondant à l'id recherché
+     * @see Jeu#getCaseOrigineDepuisCoordonneeInput()
+     * @see Jeu#getCaseDestinationDepuisCoordonneeInput()
+     * @author yohan
+     */
     public Case convertirCaseDepuisInput(String inputString, Grille grille) {
         int x;
         int y;
@@ -405,6 +588,9 @@ public class Jeu {
         return grille.getCase(x, y);
     }
 
+    /**
+     * Fonction changeant la couleur du tour
+     */
     public void changerCouleurTour() {
         if (this.tour.equals("blanc")) {
             this.tour = "noir";
@@ -413,44 +599,71 @@ public class Jeu {
         }
     }
 
+    /**
+     * Getter de {@linkplain Jeu#tour}
+     * @return {@linkplain Jeu#tour}
+     * @author yohan
+     */
     public String getTour() {
         return tour;
     }
 
+    /**
+     * Setter de {@linkplain Jeu#tour}
+     * @param string {@linkplain Jeu#tour}
+     * @author yohan
+     */
     public void setTour(String string) {
         this.tour = string;
     }
 
+    /**
+     * Fonction formattant le nom des fichiers de sauvegarde dans un format plus agréable à lire :
+     * <p>Exemple : 2015/04/12 14:35:24</p>
+     * @param fileName <code>String</code> nom du fichier
+     * @return <code>String</code> nom du fichier magnifié
+     * @author yohan
+     */
     public static String getSaveFileName(String fileName) {
         if (fileName.matches("[0-9-]")) {
-            String parsedSaveName = "";
+            StringBuilder parsedSaveName = new StringBuilder();
             String[] fileNameArray = fileName.split("");
             for (int a = 0; a < 4; a++) {
-                parsedSaveName += fileNameArray[a];
-            }  parsedSaveName += "/";
+                parsedSaveName.append(fileNameArray[a]);
+            }  parsedSaveName.append("/");
             for (int m = 4; m < 6; m++) {
-                parsedSaveName += fileNameArray[m];
-            }  parsedSaveName += "/";
+                parsedSaveName.append(fileNameArray[m]);
+            }  parsedSaveName.append("/");
             for (int j = 6; j < 8; j++) {
-                parsedSaveName += fileNameArray[j];
-            }  parsedSaveName += " ";
+                parsedSaveName.append(fileNameArray[j]);
+            }  parsedSaveName.append(" ");
             for (int h = 9; h < 11; h++) {
-                parsedSaveName += fileNameArray[h];
-            }  parsedSaveName += ":";
+                parsedSaveName.append(fileNameArray[h]);
+            }  parsedSaveName.append(":");
             for (int m = 11; m < 13; m++) {
-                parsedSaveName += fileNameArray[m];
-            }  parsedSaveName += ":";
+                parsedSaveName.append(fileNameArray[m]);
+            }  parsedSaveName.append(":");
             for (int s = 13; s < 15; s++) {
-                parsedSaveName += fileNameArray[s];
-            }  parsedSaveName += "";
-            return parsedSaveName;
+                parsedSaveName.append(fileNameArray[s]);
+            }  parsedSaveName.append("");
+            return parsedSaveName.toString();
         } else return fileName;
     }
 
+    /**
+     * Setter de {@linkplain Jeu#niveauDeDifficulte}
+     * @param niveauDeDifficulte {@linkplain Jeu#niveauDeDifficulte}
+     * @author yohan
+     */
     public void setNiveauDeDifficulte(int niveauDeDifficulte) {
         this.niveauDeDifficulte = niveauDeDifficulte;
     }
 
+    /**
+     * Getter de {@linkplain Jeu#niveauDeDifficulte}
+     * @return {@linkplain Jeu#niveauDeDifficulte}
+     * @author yohan
+     */
     public int getNiveauDeDifficulte() {
         return niveauDeDifficulte;
     }
