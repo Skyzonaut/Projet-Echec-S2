f080c4832d07d729deebf574cfc77c838a3bdbb8
yohan <yohan.folliot@gmail.com>
2021-06-07 06:04:59 +0200

    Release Version + Doc

---
+++ src/com/echec/ui/ControllerParam.java
@@ -0,0 +1,11 @@
+package com.echec.ui;
+
+/**
+ * Controlleur de la fenêtre de sélection des paramètres, n'est utilisé que pour valider
+ * le build de l'application. Sinon il ne possède aucune fonctions.
+ * Tout est géré dans {@linkplain Controller#windowParam()}
+ * @author yohan
+ */
+public class ControllerParam {
+    // https://www.youtube.com/watch?v=dQw4w9WgXcQ
+}
